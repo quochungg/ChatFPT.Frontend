e2999f0ed105df7c69a5710925da53145f985923
Khang <khanglytvb.com>
2025-02-19 19:31:34 +0700

    Create HomePage

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import ChatInput from "../components/ChatInput";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <ChatInput />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
