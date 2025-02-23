95a37a5a0adb89f3a569334580da009945f44756
Khang <khanglytvb.com>
2025-02-23 20:13:13 +0700

    update chatInput

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -5,7 +5,7 @@ const ChatInput = () => {
     <div className="text-center mt-5">
       <h2>Tôi có thể giúp gì cho bạn?</h2>
       <div className="input-group mt-3 w-50 mx-auto">
-        <input type="text" className="form-control form-control-lg" placeholder="Nhắn tin cho ChatGPT" />
+        <input type="text" className="form-control form-control-lg" placeholder="Nhắn tin cho ChatFPT" />
         <button className="btn btn-primary">Gửi</button>
       </div>
     </div>
